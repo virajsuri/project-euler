99f8ef887915e6e3148b485baeec25ce9a4abd0c
unknown <vpsuri@irusmail.com>
2020-09-23 13:40:20 -0600

    updated 35 using sieve, cleaned code

--- code/35.py
+++ code/35.py
@@ -1,41 +1,58 @@
-from isprime import isPrime
+import numpy as np
 import datetime
 start_time = datetime.datetime.now()
+
 count = 0
+length = 1000000
 
-for x in range(1,1000000,2):
+def SieveOfEratosthenes(n): 
+    primes = []
+    prime = [True for i in range(n + 1)] 
+    p = 2
+    while (p * p <= n): 
+          
+        # If prime[p] is not changed, then it is a prime 
+        if (prime[p] == True): 
+              
+            # Update all multiples of p 
+            for i in range(p * 2, n + 1, p): 
+                prime[i] = False
+        p += 1
+    prime[0]= False
+    prime[1]= False
+    # Print all prime numbers 
+    for p in range(n + 1): 
+        if prime[p]: 
+            # print(p)
+            primes.append(p)
     
-    
-    # print(x)
-    x_string = str(x)
-    if('0' in x_string[1:]
-        or '4' in x_string[1:]
-        or '6' in x_string[1:]
-        or '8' in x_string[1:]
-        ):
-        # print(x_string)
+    return primes
+
+
+primes = np.array(SieveOfEratosthenes(length))
+print('Primes generated')
+
+for x in range(1, length, 2):
+    if(('0' or '2' or '4' or '5' or '6' or '8') in str(x)):  
         continue
-
-    if(isPrime(x)):
-
+    
+    if(x in primes):
         split = (str(x))
         non_prime_found = False
         
+        #iterate through rotations
         for x in range(len(split)):
             split = split[1:] + split[:1]
 
             #if the rotation is not prime
-            if(isPrime(int(split)) == False):
+            if(int(split) not in primes):
                 non_prime_found = True
                 break
 
         if(non_prime_found == False):
             count+=1
 
-        non_prime=False
-
-print(count+1)
-
+print(count+1) #add one to account for 2 being prime
 
 end_time = datetime.datetime.now()
 print((end_time-start_time))
